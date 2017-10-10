a1da9cbb483442e2ba33f35c2e0770953d3dfe20
David Itsygin <daviditsygin@gmail.com>
2017-10-10 10:36:31 -0700

    device list fontawesome styling changes post-setup flow

--- events.js
+++ events.js
@@ -9,6 +9,9 @@ function initEvents() {
         if (ui.firstTime) {
             socket.emit('request code', {token : localStorage.getItem('token')})
         }
+        else {
+            socket.emit('request device list', {token : localStorage.getItem('token')})
+        }
         console.log('good')
     })
 
@@ -44,37 +47,37 @@ function initEvents() {
     socket.on('mobile added', function(data){
         localStorage.setItem('completedSetup', true)
         ui.firstTime = false
-        socket.emit('request device list')
+        socket.emit('request device list', {token : localStorage.getItem('token')})
     })
-    
+
     socket.on('device list', function(data){
-      
+        ui.devices = data.devices
     })
-    
+
     socket.on('request queues', function(){
       getQueues(function(clientData){
         socket.emit('got queues', {clientData: clientData})
       })
     })
-    
+
     socket.on('create lobby', function(data){
       createLobby(data.id, function(result, queueId){
         socket.emit('created lobby', {result: result, queueId: queueId})
       })
     })
-    
+
     socket.on('leave lobby', function(){
       leaveLobby(function(result){
         socket.emit('left lobby', {result: result})
       })
     })
-    
+
     socket.on('start queue', function(data){
       matchMakingSearch(function(result){
         socket.emit('started queue', {result: result})
       })
     })
-    
+
     socket.on('cancel queue', function(data){
       matchMakingCancel(function(result){
         socket.emit('cancelled queue', {result: result})
